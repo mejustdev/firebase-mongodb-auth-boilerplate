a002503e214cd0152a4221b18f77fa6699e51b0e
Me <mergorgec@gmail.com>
2021-02-15 10:42:01 +0100

    add login & register state

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,56 +1,43 @@
 import firebase from '../firebase';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import LoginRegisterForm from '../components/LoginRegisterForm';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPass, setLoginPass] = useState('');
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPass, setRegisterPass] = useState('');
   const router = useRouter();
 
   const register = () => {
-    //
+    console.log(registerEmail, registerPass);
   };
 
   const login = () => {
-    //
+    console.log(loginEmail, loginPass);
   };
-
-  const loginRegisterForm = (buttonName) => (
-    <div className='col-md-6'>
-      <div className='form-group'>
-        <label>Email</label>
-        <input
-          type='email'
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          className='form-control'
-        />
-        <small>We'll never share your email</small>
-      </div>
-
-      <div className='form-group'>
-        <label>Password</label>
-        <input
-          type='email'
-          onChange={(e) => setPassword(e.target.value)}
-          value={pass}
-          className='form-control'
-        />
-      </div>
-
-      <div>
-        <button onClick={register} className='btn btn-primary'>
-          {buttonName}
-        </button>
-      </div>
-    </div>
-  );
 
   return (
     <div className='container'>
       <h2 className='text-center pt-4 display-4'>Login / Register</h2>
       <div className='row'>
-        {loginRegisterForm('Login')} {loginRegisterForm('Register')}
+        <LoginRegisterForm
+          email={loginEmail}
+          setEmail={setLoginEmail}
+          pass={loginPass}
+          setPass={setLoginPass}
+          handleSubmit={login}
+          buttonName='Login'
+        />
+        <LoginRegisterForm
+          email={registerEmail}
+          setEmail={setRegisterEmail}
+          pass={registerPass}
+          setPass={setRegisterPass}
+          handleSubmit={register}
+          buttonName='Register'
+        />
       </div>
     </div>
   );
