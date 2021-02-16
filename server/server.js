a7d8a2061a12b9325fb5a1496a7466a410264350
Me <mergorgec@gmail.com>
2021-02-16 16:32:11 +0100

    add controllers & routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import { readdirSync } from 'fs';
 const morgan = require('morgan');
 
 require('dotenv').config();
@@ -23,12 +24,10 @@ mongoose
 app.use(cors());
 app.use(morgan('dev'));
 
+// routes middleware
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+
 // routes
-app.get('/api/:message', (req, res) => {
-  res.json({
-    message: `Your message is received ${req.params.message}`,
-  });
-});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
