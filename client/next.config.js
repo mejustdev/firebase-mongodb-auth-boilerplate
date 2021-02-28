c1e545be56525ffbfaa287454824da14e84172a9
Me <mergorgec@gmail.com>
2021-02-28 19:27:14 +0100

    ready to deploy

---
+++ client/next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    api: 'http://localhost:8000/api',
+    passwordResetRedirect: 'http://localhost:3000/login',
+    NEXT_PUBLIC_FB_API_KEY: 'AIzaSyC0AlyJ3sJm8AxzxS14Ph8iEVxKjE8RbW8',
+    NEXT_PUBLIC_FB_AUTH_DOMAIN: 'firemon-51310.firebaseapp.com',
+    FB_PROJECT_ID: 'firemon-51310',
+    FB_STORAGE_BUCKET: 'firemon-51310.appspot.com',
+    FB_MESSAGING_SENDER_ID: '246819088340',
+    FB_APP_ID: '1:246819088340:web:c7d027163932e434499841',
+    DATABASE:
+      'mongodb+srv://mergorgec:7wfDlCOI7EZQgANN@firemon.2cdvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    PORT: 8000,
+  },
+};
